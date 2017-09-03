b83ccecbe83675374c12f4f1bcfcbc259d782073
windmill mc <fujitsubo1324@gmail.com>
2017-09-04 00:47:25 +0900

    とりあえず完成

---
+++ FileGetter/FileGetter/FileNameGetter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using FileUtil;
+//C:\Users\fujit\Downloads\news.txt
+namespace FileNameGetter
+{
+	class FileNameGetter
+	{
+		[STAThread]
+		static void Main()
+		{
+			var str = Clipboard.GetText();
+			var strList = str.Split('\n');
+			var newStrList = new List<String>();
+
+			//---StringListをパースしてファイル名だけのリストをnewStrListに格納する
+			for(int i = 0; i < strList.Length; i++)
+			{
+				var fileName = strList[i].GetFileName();
+				fileName.WriteConsole();
+				
+				if(i != strList.Length-1)
+					newStrList.Add(fileName+",");
+				//---最後の要素には","を入れない
+				else
+					newStrList.Add(fileName);
+			
+			}
+			
+			//---newStrListを一つのstringに格納
+			string setText="";
+			foreach(var item in newStrList)
+			{
+				setText += item;
+			}
+			Clipboard.SetText(setText);
+			Console.Read();
+		}
+	}
+}
+
---
+++ FileGetter/FileGetter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+using System.Collections.Generic;
+using System.Linq;
+using FileUtil;
+namespace FileCopy
+{
+	//C:\Users\fujit\Downloads\news.txt
+	class Program
+	{
+		static void Main()
+		{
+			Console.Write("コピー元のファイルのパスを入力してください\n");
+			String path = Console.ReadLine();
+			var file = File.OpenRead(path);
+			StreamReader str = new StreamReader(file);
+
+			List<string> inputStrings = new List<string>();
+			String inputed = "";
+			while(inputed != "End")
+			{
+				Console.Write("新しく作成するファイルの名前を入力してください\n");
+				inputed = Console.ReadLine();
+				List<string> inputList = inputed.Split(',').ToList();
+
+				while(inputList.Count!=0)
+				{
+					var newPath = '\\'+inputList[0] +'.'+ path.Substring(path.LastIndexOf('\\')).Split('.')[1];
+					var newPath2 = path.Remove(path.LastIndexOf('\\')) +newPath;
+					if(inputList[0]!="End")
+					{
+						Console.Write($"{newPath2}を作成しました\n");
+						File.Copy(path,newPath2);
+						inputList.RemoveAt(0);
+				
+					}
+					else
+					{
+						break;
+					}
+				}
+				
+
+				
+			}
+			Console.Read();
+		}
+	}
+
+
+	
+
+}
+
+namespace FileUtil
+{
+	public static class ConsoleKeyInfoEx
+	{
+		public static void WriteConsole(this ConsoleKeyInfo info)
+		{
+			Console.Write(info.Key);
+		}
+
+		public static void WriteConsole(this List<string> info)
+		{
+			info.ForEach(X=>Console.Write(X));
+		}
+	}
+}
